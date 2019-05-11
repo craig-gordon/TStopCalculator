afcc8e5eb7d5cb85d7332902333f3613f47bfdbe
Craig Gordon <cyghfer@gmail.com>
2019-05-11 19:00:05 -0400

    Add splash screen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,9 +1,17 @@
+import { Easing, Animated } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import Splash from './Splash';
 import Input from './Input';
 import Output from './Output';
 
 const AppNavigator = createStackNavigator(
   {
+    Splash: {
+      screen: Splash,
+      navigationOptions: {
+        header: null
+      }
+    },
     Input: {
       screen: Input,
       navigationOptions: {
@@ -18,7 +26,30 @@ const AppNavigator = createStackNavigator(
     }
   },
   {
-    initialRouteName: 'Input'
+    initialRouteName: 'Splash',
+    transitionConfig: () => {
+      return {
+        transitionSpec: {
+          duration: 750,
+          easing: Easing.out(Easing.poly(4)),
+          timing: Animated.timing,
+          useNativeDriver: true
+        },
+        screenInterpolator: sceneProps => {
+          const { layout, position, scene } = sceneProps;
+
+          const thisSceneIndex = scene.index;
+          const width = layout.initWidth;
+
+          const translateX = position.interpolate({
+            inputRange: [thisSceneIndex - 1, thisSceneIndex],
+            outputRange: [width, 0]
+          });
+
+          return { transform: [{ translateX }] };
+        }
+      };
+    }
   }
 );
 
---
+++ Splash.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#404041',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  header1: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 48,
+    marginBottom: 0,
+    paddingBottom: 0
+  },
+  header2: {
+    color: '#FFFFFF',
+    fontSize: 48,
+    marginBottom: 30
+  },
+  subheader: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    marginBottom: 100
+  }
+});
+
+export default class Splash extends React.Component {
+  componentDidMount() {
+    setTimeout(() => this.props.navigation.navigate('Input'), 2500);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header1}>ZERO</Text>
+        <Text style={styles.header2}>OPTIK</Text>
+        <Text style={styles.subheader}>T-STOP CALCULATOR</Text>
+      </View>
+    );
+  }
+}
